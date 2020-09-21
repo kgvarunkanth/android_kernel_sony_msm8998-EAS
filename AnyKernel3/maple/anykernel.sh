30ab209f61c9762edea29298829f894a18b9afe7
Andrea <linckandrea01@gmail.com>
2020-09-21 09:54:13 +0200

    anykernel: push module in the old way

--- AnyKernel3/maple/anykernel.sh
+++ AnyKernel3/maple/anykernel.sh
@@ -7,7 +7,7 @@ properties() { '
 kernel.string=pop kernel for maple by linckandrea @ xda-developers
 do.devicecheck=1
 do.modules=1
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=maple
